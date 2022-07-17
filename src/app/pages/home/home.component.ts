f4c085fd9d0f4fe99f02be36c493c7217cc7d3a2
Jose Araque <ja.araque14@gmail.com>
2022-07-17 16:40:35 -0500

    feat table

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonModel } from 'src/app/domain/model/pokemon-model';
 import { PokemonService } from 'src/app/domain/services/pokemon/pokemon.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { PokemonService } from 'src/app/domain/services/pokemon/pokemon.service'
 export class HomeComponent implements OnInit {
 
   public title = 'Listado de Pokemon';
+  public pokemons: PokemonModel[];
 
   constructor(
     private pokemonService: PokemonService
@@ -21,12 +23,17 @@ export class HomeComponent implements OnInit {
   getPokemons(){
     this.pokemonService.getPokemonByIdAutor(1).subscribe(
       res => {
+        this.pokemons = res;
         console.log(res);
       },
       err => {
         console.log(err);
       }
     );
+  }
+
+  searchUser(){
+    console.log('llamó')
   }
 
 }
